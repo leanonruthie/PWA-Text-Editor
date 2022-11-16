f20885ff1de1223cf678fdab12d2b38aae659772
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-11-16 16:17:10 -0500

    continual compare/contrast and modification

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,5 @@
+// Work Reference: Code and comment incorporated from 28-Stu_Mini-Project 
+
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -27,4 +29,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  // filtered requests > cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Cache storage name
+    cacheName: 'asset-cache',
+    plugins: [
+      // Caching responses with maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
